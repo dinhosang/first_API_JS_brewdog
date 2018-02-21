658a83e7f3fd9eb90f851ba6fa17461e4c3669c2
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-02-21 23:08:58 +0000

    tried to go singleton route but does not work due to gap between the return of the instance, and the return of the request to the API, gone a more regular route

--- public/app.js
+++ public/app.js
@@ -1,6 +1,42 @@
-const app = function () {
-  const beers = new BeerCollection()
-  console.log(beers);
+const onPageLoad = function () {
+  requestToAPI()
 };
 
-document.addEventListener('DOMContentLoaded', app);
+const requestToAPI = function() {
+  const request = new XMLHttpRequest()
+  request.open('GET', "https://api.punkapi.com/v2/beers")
+  request.addEventListener('load', convertJson)
+  request.send()
+}
+
+const convertJson = function() {
+  const jsonString  = this.responseText
+  const beers       = JSON.parse(jsonString)
+
+  populateList(beers)
+}
+
+const populateList = function(beers) {
+  const mainList = document.getElementById('list_of_beers')
+  beers.forEach(beer => createListItem(beer, mainList))
+}
+
+const createListItem = function(beer, mainList) {
+  const newListItemName = document.createElement('li')
+  const newListItemImg  = document.createElement('li')
+  const newImg    = document.createElement('img')
+  const breakLine = document.createElement('br')
+
+  newListItemName.innerText = beer.name
+
+  newImg.width  = '50'
+  newImg.src    = beer.image_url
+
+  newListItemImg.appendChild(newImg)
+
+  mainList.appendChild(newListItemName)
+  mainList.appendChild(newListItemImg)
+  mainList.appendChild(breakLine)
+}
+
+document.addEventListener('DOMContentLoaded', onPageLoad);
--- public/models/beerCollection.js
+++ public/models/beerCollection.js
@@ -1,26 +1,32 @@
 let instance = null
 
+// this doesn't work as the constructor returns before the
+// request is completed. Can't find a workaround it.
+// would need to be able to delay the return until this event
+// happened
 const BeerCollection = function(){
-
+  // // to test singleton
+  // console.log(!instance);
   if(!instance){
-    instance = this
     // // to test singleton
     // this.time = new Date()
     this.requestToAPI()
   }
 
   return instance
+
+  this.beers = []
 }
 
 BeerCollection.prototype.requestToAPI = function () {
   const request = new XMLHttpRequest()
   request.open('GET', "https://api.punkapi.com/v2/beers")
-  request.addEventListener('load', this.convertJson.bind(request, this))
+  request.addEventListener('load', this.convertJson.bind(this, request))
   request.send()
 }
 
-BeerCollection.prototype.convertJson = function(beerCollection) {
-  const jsonString  = this.responseText
-  const beers       = JSON.parse(jsonString)
-  beerCollection.beers = beers
+BeerCollection.prototype.convertJson = function(request) {
+  const jsonString  = request.responseText
+  this.beers        = JSON.parse(jsonString)
+  instance = this
 }
